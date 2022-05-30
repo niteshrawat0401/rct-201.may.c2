36bcfb5c81aadedee7322a47cbd8c051e97d9ee4
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-05-30 17:58:15 +0530

    set mistake

--- c2/src/Components/Input.jsx
+++ c2/src/Components/Input.jsx
@@ -1,4 +1,5 @@
-// import Eye from "./eve.svg"
+
+import pic from "../Components/eye.svg"
 export const Input = ({
   type,
   size,
@@ -7,16 +8,13 @@ export const Input = ({
   rightLogoOnClick,
   onChange,
 }) => {
+  
   return (
     <div className="inputContainer">
       {/* Add Right Logo */}
-      <input className={`input
-      
-      ${type="text"} 
 
-    ${rightLogo="./eve.svg"}
-
-      `} />
+      <input className={`input`} type={"password"} size={size} onChange={onChange}/>
+      <img src={pic} width={"20px"} alt="" />
     </div>
   );
 };
--- c2/src/Components/Pagination.jsx
+++ c2/src/Components/Pagination.jsx
@@ -1,7 +1,18 @@
+import { Input } from "./Input";
+import App from "../App.css"
 export const Pagination = ({ total, selected, onPageChange }) => {
-
-
   return <div className="pageContainer">
-    
+    {new Array(total).fill(1).map((_, index, arr) => {
+    return(
+     
+
+      <input
+      className="inPut"
+      key={index}
+      value={index+1}
+      />
+    )
+  })}
   </div>;
+  
 };
