1ed4781dd82db564ced4fd1584aaec41e152f3d2
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-05-30 17:27:44 +0530

    add data

--- c2/src/App.jsx
+++ c2/src/App.jsx
@@ -1,15 +1,24 @@
 import { Input } from "./Components/Input";
 import { Image } from "./Components/Image";
 import { Pagination } from "./Components/Pagination";
+import { useState } from "react";
 
 function App() {
+  const [value ,setValue] = useState("")
+
   return (
     <div className="container">
-      <Input />
+      <Input  type={"text"} size={"md"} onChange={(e)=>{setValue(e.target.value)}}/>
 
-      <Image />
+      <Image src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVA_HrQLjkHiJ2Ag5RGuwbFeDKRLfldnDasw&usqp=CAU"}
+      alt={"username"}
+      borderRadius={100}
+      width={200}
+      height={200}
+      fit={"cover"}
+      />
 
-      <Pagination />
+      <Pagination total={10}  />
     </div>
   );
 }
--- c2/src/Components/Image.jsx
+++ c2/src/Components/Image.jsx
@@ -1,16 +1,12 @@
-
 export const Image = ({ src, alt, borderRadius, width, height, fit }) => {
-
-  const picture = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVA_HrQLjkHiJ2Ag5RGuwbFeDKRLfldnDasw&usqp=CAU"
   return (
     <div>
-      <img src={picture} alt={"eye"} 
-
-      borderRadius={"100px"} 
-
-      width="100px" height={"100x"}
-      
-       fit={"cover"}/>
+      <img 
+      src={src} 
+      alt={alt}
+       style={{borderRadius:borderRadius,width:width,height:height}} 
+       
+       />
     </div>
   );
 };
